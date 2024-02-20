ea6c7362c98647c860e92c1b12dec3b19678a3d6
Ludovico Bianchi <lbianchi@lbl.gov>
2024-02-20 15:45:40 -0600

    Reduce unnecessary output during Quarto installation

--- install-quarto.sh
+++ install-quarto.sh
@@ -9,7 +9,7 @@ BIN_DIR="$HOME/.local/bin"
 filename="quarto-${QUARTO_RELEASE}-linux-amd64.tar.gz"
 
 mkdir -p "$INSTALL_DIR" "$BIN_DIR"
-wget "https://github.com/quarto-dev/quarto-cli/releases/download/v${QUARTO_RELEASE}/${filename}"
+wget --quiet "https://github.com/quarto-dev/quarto-cli/releases/download/v${QUARTO_RELEASE}/${filename}"
 tar -C "$INSTALL_DIR" -xvzf "$filename"
 rm "$filename"
 ln -s "${INSTALL_DIR}/quarto-${QUARTO_RELEASE}/bin/quarto" "${BIN_DIR}/quarto"
